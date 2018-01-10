ef454391ae6ad4b73639b08224791b8381315ccb
kwichmann <kristian.wichmann@gmail.com>
2018-01-10 15:34:32 +0100

    Make event log into traces

---
+++ test.R
@@ -0,0 +1,9 @@
+source("heuristics.R")
+
+fake <- data.frame(id = sample(1:10, 1000, replace = T),
+                   event = sample(1:5, 1000, replace = T),
+                   timestamp = runif(1000) * 100)
+
+fake <- sortLog(fake)
+
+View(getAllTraces(fake))
