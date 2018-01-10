2a09eb569543012b9836df68f5c90b376d282228
kwichmann <kristian.wichmann@gmail.com>
2018-01-10 15:50:53 +0100

    Renaming files and small refactoring

---
+++ logToTraces.R
@@ -0,0 +1,28 @@
+# Functions for turning event log into a data frame of traces
+
+# Get dictionary of event names
+getDictionary <- function(w, events = "event") {
+  unique(w[,events])
+}
+
+# Sort event log by time stamp
+sortLog <- function(w, timestamps = "timestamp") {
+  w[order(w[[timestamps]]),]
+}
+
+# Get the trace corresponding to id from a *time sorted* event log
+getTrace <- function(w, id, ids = "id", events = "event") {
+  tr <- w[w[[ids]] == id,]
+  tr[[events]]
+}
+
+# Get data frame of traces
+getAllTraces <- function(w, ids = "id", events = "event", timestamps = "timestamp") {
+  dict <- getDictionary(w, events)
+  w <- sortLog(w, timestamps)
+  traces <- mapply(getTrace, list(w), dict, ids, events)
+  # Hack to insert list of vectors into data frame
+  allTraces <- data.frame(id = as.factor(dict), trace = 0)
+  allTraces$trace <- traces
+  allTraces
+}
--- test.R
+++ test.R
@@ -1,4 +1,4 @@
-source("heuristics.R")
+source("logToTraces.R")
 
 fake <- data.frame(id = sample(1:10, 1000, replace = T),
                    event = sample(1:5, 1000, replace = T),
